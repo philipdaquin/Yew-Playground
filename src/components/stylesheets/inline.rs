84f9ac0e9a9e7f1478c61f798405e907fd1f51cc
philidaquin <philipdaquin20@gmail.com>
2022-02-06 12:58:18 +0800

    fontweiight

--- src/components/stylesheets/inline.rs
+++ src/components/stylesheets/inline.rs
@@ -5,12 +5,14 @@ pub fn inline() -> Html {
     // In line styling in rust 
     let heading = {"
         font-size: 72px; 
+        font-weight: 2rem;
         color: blue;"
     };
     html! { 
         <>
             <div>
-                <h1 style={heading}>{"Inline"}</h1>
+                <h1 style={heading}>{"Inline"}</h1> 
+
             </div>
         </>
     }
