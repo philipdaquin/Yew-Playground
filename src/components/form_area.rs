a3d27e79d1de60b94453a5d19fe6c8368486a501
philidaquin <philipdaquin20@gmail.com>
2022-02-06 17:32:02 +0800

    form area

--- src/components/form_area.rs
+++ src/components/form_area.rs
@@ -1,11 +1,12 @@
-use yew::{prelude::*, NodeRef};
+use yew::{prelude::*, NodeRef, Html};
+use web_sys::{Text, HtmlInputElement};
 
 pub struct FormArea { 
     pub value: String, 
     pub input_ref: NodeRef
 }
 #[derive(Properties, PartialEq, Clone)]
-pub struct TextAReaProps { 
+pub struct TextAreaProps { 
     #[prop_or_default]
     pub name: String,
 
@@ -18,8 +19,6 @@ pub struct TextAReaProps {
     #[prop_or_default]
     pub oninput: Callback<String>
 }
-
-
 // Signal the input element has changed
 pub enum TextMsg { 
     Changed(String),
@@ -28,7 +27,7 @@ pub enum TextMsg {
 
 impl Component for FormArea { 
     type Message = TextMsg;
-    type Properties = TextAReaProps;
+    type Properties = TextAreaProps;
 
     fn create(ctx: &Context<Self>) -> Self {
         let value = ctx.props().value.clone();
@@ -41,24 +40,47 @@ impl Component for FormArea {
         match msg { 
             TextMsg::Changed(input) => { 
                 self.value = input.clone();
-                ctx.props().onchange.emit(input)
+                ctx.props().onchange.emit(input);
+                false
             },
             TextMsg::Input(input) => { 
-
+                ctx.props().oninput.emit(input);
+                if let Some(val) = self.extract_value() { 
+                    self.value = val.clone();
+                    ctx.props().onchange.emit(val);
+                }
+                false
             }
         }
-        true 
-    }
-    fn changed(&mut self, ctx: &Context<Self>) -> bool {
-        false
     }
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let input_ref = self.input_ref.clone();
+        
+        let oninput = ctx
+            .link()
+            .callback(|input: InputEvent| TextMsg::Input(input.data().unwrap_or_default()));
+       let onchange = ctx.link().batch_callback(move |_| { 
+           input_ref.cast::<HtmlInputElement>().map(|input| TextMsg::Changed(input.value()))
+       });
+        
         html! {
             <>
-                <div></div>
+                <textarea
+                ref={self.input_ref.clone()}
+                name={ctx.props().name.clone()}
+                value={ctx.props().value.clone()}
+                onchange={onchange}
+                oninput={oninput}
+                ></textarea>
             </>
         }
-
     }
-
+}
+impl FormArea { 
+    fn extract_value(&self) -> Option<String> { 
+        self.input_ref
+        .cast::<HtmlInputElement>()
+        .map(|input| input.value()) 
+    }
+    
 }
