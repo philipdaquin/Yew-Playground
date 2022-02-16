7bb5a1bcc2b09215b525337f592f169e2d073980
philipdaquin <philipdaquin20@gmail.com>
2022-02-17 07:20:06 +0800

    Use reducer complex state action

--- src/use_reducer/countertwo.rs
+++ src/use_reducer/countertwo.rs
@@ -4,21 +4,31 @@ use std::rc::Rc;
 
 pub enum Action { 
     Increment, 
+    IncrementBy(i32),
     Decrement,
-    Reset
+    DecrementBy(i32),
+    Reset,
+
+    Increment2, 
+    IncrementBy2(i32),
+    Decrement2,
+    DecrementBy2(i32),
+    Reset2,
 
 }
 //  Reducer's State
 #[derive(Clone, Debug, PartialEq)]
 pub struct Counter { 
     pub value: i32,
-    pub default: i32
+    pub second_counter: i32,
+    pub default: i32, 
 }
 
 impl Default for Counter { 
     fn default() -> Counter { 
         Self { 
             value: 0,
+            second_counter: 0,
             default: 0
         }
     }
@@ -33,16 +43,27 @@ impl Reducible for Counter  {
     /// function which computes a new state that is returned and the component is re-rendered
     fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> { 
        let curr = match action { 
-           Increment => {self.value + 1},
-           Decrement => {self.value -1},
-           Reset => {self.default}
+            Action::Increment => { self.value + 1},
+            Action::Decrement => {self.value -1},
+            Action::Reset => {self.default},
+            Action::IncrementBy(x)  => {self.value + x},
+            Action::DecrementBy(x) => {self.value - x},
+            Action::Increment2 => {self.second_counter + 1 },
+            Action::Decrement2 => {self.second_counter -1},
+            Action::Reset2 => {self.default},
+            Action::IncrementBy2(x)  => {self.second_counter + x},
+            Action::DecrementBy2(x) => {self.second_counter - x}
+           
        };
        Counter  { 
            value: curr,
+           second_counter: curr,
            default: self.default
        }.into()
     }
 }
+
+
 
 #[function_component(CounterTwo)]
 pub fn countertwo() -> Html {
@@ -61,15 +82,51 @@ pub fn countertwo() -> Html {
         let counter = counter.clone();
         Callback::from(move |_| counter.dispatch(Action::Reset))
     };
+    let decrement_by = { 
+        let counter = counter.clone();
+        Callback::from(move |e: MouseEvent| counter.dispatch(Action::DecrementBy(5)))
+    };
+    let increment_by = { 
+        let counter = counter.clone();
+        Callback::from(move |e: MouseEvent| counter.dispatch(Action::IncrementBy(5)))
+    };
 
+
+    let increment2 ={ 
+        let counter = counter.clone();
+        Callback::from(move |_|  counter.dispatch(Action::Increment2))
+    };
+    let decrement2 = { 
+        let counter = counter.clone();
+        Callback::from(move |_| counter.dispatch(Action::Decrement2))
+    };
+    let reset2 = { 
+        let counter = counter.clone();
+        Callback::from(move |_| counter.dispatch(Action::Reset2))
+    };
+
+
+
+
+
+
+   
     
     html! {
         <>
             <div>
-                <h1>{counter.value}</h1>
+                <h1>{"First Counter: "}{counter.value}{"Second Counter: "}{counter.second_counter}</h1>
                 <button onclick={increment}>{"Increment"}</button>
                 <button onclick={decrement}>{"Decrement"}</button>
                 <button onclick={reset}>{"Reset"}</button>
+                <button onclick={decrement_by}>{"Decrement by 5"}</button>
+                <button onclick={increment_by}>{"Increment by 5"}</button>
+
+
+
+                <button onclick={increment2}>{"Increment2"}</button>
+                <button onclick={decrement2}>{"Decrement"}</button>
+                <button onclick={reset2}>{"Reset"}</button>
             </div>
         </>
     }
