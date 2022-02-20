0877d1f037df085b0a76bed811e5e90d9137aa59
philipdaquin <philipdaquin20@gmail.com>
2022-02-20 12:21:15 +0800

    cleaning up

--- src/router/cartprovider.rs
+++ src/router/cartprovider.rs
@@ -3,9 +3,6 @@ use yew::prelude::*;
 
 
 
-
-
-
 #[derive(PartialEq, Properties, Clone)]
 pub struct CartProviderProps { 
     #[prop_or_default]
--- src/router/home.rs
+++ src/router/home.rs
@@ -1,45 +1,4 @@
-use std::rc::Rc;
-
-use reqwasm::websocket::Message;
 use yew::prelude::*;
-use crate::components::{
-    lifecycleA::LifeCycleA,
-    table::Table,
-    purecomp::PureComp,
-    regcomp::RegComp,
-   // noderefs::Ref,
-   portaldemo::PortalDemo,
-   errorhandling::Hero,
-   errorboundary::ErrorBoundary,
-   mousemovement::MouseMovement
-};
-use crate::service::postform::*;
-use crate::higherordercomp::{
-    clickcounter::ClickCounter,
-    hovercounter::HoverCount,
-    with_counter::OriginalComponent,
-};
-use crate::renderprop::{clickcountertwo::ClickCounterTwo,
-    hovercountertwo::HoverCounterTwo,
-    user::User
-};
-use crate::service::{postform::*, };
-use crate::hooks::{};
-use crate::hooks::{hookcounter::*, hookcounter2::*, hookcounter3::*};
-use crate::use_effect::{
-    hookcounterone::HookCounterOne,
-    mousecontainer::MouseContainer
-};
-
-use crate::fetching_data::datafetching::DataFetching;
-
-// use crate::usecontext::{
-//     compc::CompC,
-//     compe::CompE,
-//     compf::CompF,
-// };
-
-
 
 pub struct Home;
 
@@ -60,83 +19,8 @@ impl Component for Home {
         html! {
             <>
                 <div>
-
-
-                    <DataFetching/>
-                    //  Reducer and Context
-
-                    //<MouseContainer/>
-                    // <HookCounter/>
-                    // <HookCounterTwo />
-                    // <HookCounterThree />
-                    // <HookCounterOne/>
-                    // <MouseMovement/>
-                    //<PostForm />
-                    // <ClickCounterTwo />
-                    // <HoverCounterTwo />
-                    //<User name={}/>
+                    
                 </div>
-                    // <ClickCounter />
-                    // <HoverCount />
-                    // < OriginalComponent/>
-                    // <ErrorBoundary >
-                    //     <Hero name={"Man"}/>
-                    //     <Hero name={"Man"}/>
-                    //     <Hero name={"Man"}/>
-                    // </ ErrorBoundary>
-
-                    // <ErrorBoundary> 
-                    //     <Hero name={"Joker"}/>
-                    // </ErrorBoundary>
-                    
-                    // <ErrorBoundary> 
-                    //     <Hero name={"hello"}/>
-                    // </ErrorBoundary>
-                    
-                    // <ErrorBoundary> 
-                    //     <Hero name={"hello"}/>
-                    // </ErrorBoundary>   
-                    // <PortalDemo />
-                    // <Hero name={"Hello"}/>                   
-                    // <div class="root"></div>
-                    // <div id="portal-root"></div>
-                    // <PostList />
-                    // <Ref />
-                    // <PureComp />
-                    // <RegComp />
-                    //  <Table />
-                    //  <LifeCycleA />
-                    // <FormInput />
-                    // <FormArea />
-                    // <Inline/>
-                    // <StyleSheets styles={true} />
-                    // <NameList/>
-                    // <Person  id ={1} name={"Philip"} age={21} skill={"Rust"}/>
-                    // <Person  id={1} name={"Philip"} age={31} skill={"Substrate"}/>
-                    // <Conditional/>
-                    //<ParentComponent prop1={"Lorem"} prop2={"Ipsum"}/>
-                    //<EventBind /> 
-                    // <FunctionClick />
-                    // <ClassClick />
-                    // <Counter/>
-                    // <Message/>
-                    // <div class="home">
-                    // <Hello name={"WALICE"} num={2}>
-                    //     <p>{"THis is Children Props "}</p>
-                    //     <p>{"THis is Children Props "}</p>
-                    //     <p>{"THis is Children Props "}</p>
-                    //     <p>{"THis is Children Props "}</p>
-                    //     <button>{"Hello"}</button>
-                    // </Hello>
-                    // <Class name={"This Is Coming From a Impl Components"} num={2012}>
-                    //     <p>{"sdasdasd"}</p>
-                    //     <p>{"This is oming from a shared property "}</p>
-                    // </Class>
-                    //
-                    //     <img class="profile-picture" src="assets/images/avatar.jpg" alt="ShironCat's avatar" />
-                    //     <h1>{ "Hello, World!" }</h1>
-                    //     <img src="https://http.cat/404.jpg" />
-                    //</div>
             </>
         }
     }
